153a03f9d64490f8199b5c3ec56cbe257cd8eac2
mach64 <f20170184@hyderabad.bits-pilani.ac.in>
2018-10-02 00:27:38 +0530

    Switch to spaces for indentation
    To maintain the uniform "4 char" indentation, use spaces instead of tabs

--- main.cpp
+++ main.cpp
@@ -5,73 +5,73 @@
 using namespace std;
 
 bool is_operand(char token){
-	// Assumes operands are lowercase alphabets
-	if(token>='a'&&token<='z')
-		return true;
-	return false;
+    // Assumes operands are lowercase alphabets
+    if(token>='a'&&token<='z')
+        return true;
+    return false;
 }
 
 bool is_operator(char token){
-	const char VALID_OPERATORS[] = {'~', 'V', '^', '>'};
-	for(char op: VALID_OPERATORS)
-		if(token==op)
-			return true;
-	return false;
+    const char VALID_OPERATORS[] = {'~', 'V', '^', '>'};
+    for(char op: VALID_OPERATORS)
+        if(token==op)
+            return true;
+    return false;
 }
 
 int op_priority(char op){
-	switch (op){
-		case '~': return 4;
-		case 'V':
-		case '^': return 3;
-		case '>': return 2;
-		default: return -1;
-	}
+    switch (op){
+        case '~': return 4;
+        case 'V':
+        case '^': return 3;
+        case '>': return 2;
+        default: return -1;
+    }
 }
 
 bool right_assoc(char op){
-	if(op=='>')
-		return true;
-	return false;
+    if(op=='>')
+        return true;
+    return false;
 }
 
 string infix_to_postfix(string infix_exp){
-	stack<char> op_stack;
-	string postfix="";
-	for(char token:infix_exp){
-		if(is_operand(token)){
-			postfix.push_back(token);
-		}
-		else if(is_operator(token)){
-			while(!op_stack.empty() && 
-			      (op_priority(op_stack.top())>op_priority(token) ||
-				   (op_priority(op_stack.top())==op_priority(token)&&!right_assoc(op_stack.top()))
-				  ) &&
-				  op_stack.top()!='('){
-				postfix.push_back(op_stack.top());
-				op_stack.pop();
-			}
-			op_stack.push(token);
-		}
-		else if(token=='('){
-			op_stack.push(token);
-		}
-		else if(token==')'){
-			while(op_stack.top()!='('){
-				postfix.push_back(op_stack.top());
-				op_stack.pop();
-			}
-			op_stack.pop();
-		}
-	}
-	while(!op_stack.empty()){
-		postfix.push_back(op_stack.top());
-		op_stack.pop();
-	}
-	return postfix;
+    stack<char> op_stack;
+    string postfix="";
+    for(char token:infix_exp){
+        if(is_operand(token)){
+            postfix.push_back(token);
+        }
+        else if(is_operator(token)){
+            while(!op_stack.empty() && 
+                  (op_priority(op_stack.top())>op_priority(token) ||
+                   (op_priority(op_stack.top())==op_priority(token)&&!right_assoc(op_stack.top()))
+                  ) &&
+                  op_stack.top()!='('){
+                postfix.push_back(op_stack.top());
+                op_stack.pop();
+            }
+            op_stack.push(token);
+        }
+        else if(token=='('){
+            op_stack.push(token);
+        }
+        else if(token==')'){
+            while(op_stack.top()!='('){
+                postfix.push_back(op_stack.top());
+                op_stack.pop();
+            }
+            op_stack.pop();
+        }
+    }
+    while(!op_stack.empty()){
+        postfix.push_back(op_stack.top());
+        op_stack.pop();
+    }
+    return postfix;
 }
 
 int main(){
-	cout<<infix_to_postfix("p^q>q");
-	return 0;
+    cout<<infix_to_postfix("p^q>q");
+    return 0;
 }
