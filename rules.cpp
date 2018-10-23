c620addb1feec2cfcc5c8569f17e4fe0afb8da54
mach64 <f20170184@hyderabad.bits-pilani.ac.in>
2018-10-23 19:14:16 +0530

    refactor: Make if conditionals more readable

--- rules.cpp
+++ rules.cpp
@@ -27,28 +27,26 @@ bool check_validity(ProofLine* newline){
 }
 
 bool check_valid_prem(ProofLine* newline){
-    if(newline->line1 or newline->line2)
-        return false;
-    return true;
+    return !(newline->line1 or newline->line2);
 }
 
 bool check_valid_and_i(ProofLine* newline){
-    if(!newline->line1 or !newline->line2)
+    if(!newline->line1 or !newline->line1->is_valid_formula)
         return false;
-    if(!newline->line1->is_valid_formula or !newline->line2->is_valid_formula)
+    if(!newline->line2 or !newline->line2->is_valid_formula)
         return false;
     return newline->formula->left->get_inorder() == newline->line1->formula->get_inorder() and
            newline->formula->right->get_inorder() == newline->line2->formula->get_inorder();
 }
 
 bool check_valid_and_e1(ProofLine* newline){
-    if(newline->line2 or !newline->line1 or !newline->line1->is_valid_formula or !(newline->line1->formula->data == AND))
+    if(newline->line2 or !newline->line1 or !newline->line1->is_valid_formula or newline->line1->formula->data != AND)
         return false;
     return newline->line1->formula->left->get_inorder() == newline->formula->get_inorder();
 }
 
 bool check_valid_and_e2(ProofLine* newline){
-    if(newline->line2 or !newline->line1 or !newline->line1->is_valid_formula or !(newline->line1->formula->data == AND))
+    if(newline->line2 or !newline->line1 or !newline->line1->is_valid_formula or newline->line1->formula->data != AND)
         return false;
     return newline->line1->formula->right->get_inorder() == newline->formula->get_inorder();
 }
@@ -66,14 +64,22 @@ bool check_valid_or_i2(ProofLine* newline){
 }
 
 bool check_valid_impl_e(ProofLine* newline){
-    if(!newline->line2 or !newline->line1 or !newline->line1->is_valid_formula or !newline->line2->is_valid_formula or !(newline->line1->formula->data == IMPL))
+    if(!newline->line1 or !newline->line1->is_valid_formula)
+        return false;
+    if(!newline->line2 or !newline->line2->is_valid_formula)
+        return false;
+    if(newline->line1->formula->data != IMPL)
         return false;
     return newline->line1->formula->left->get_inorder() == newline->line2->formula->get_inorder() and
            newline->line1->formula->right->get_inorder() == newline->formula->get_inorder();
 }
 
 bool check_valid_mt(ProofLine* newline){
-    if(!newline->line2 or !newline->line1 or !newline->line1->is_valid_formula or !newline->line2->is_valid_formula or !(newline->line1->formula->data == IMPL))
+    if(!newline->line1 or !newline->line1->is_valid_formula)
+        return false;
+    if(!newline->line2 or !newline->line2->is_valid_formula)
+        return false;
+    if(newline->line1->formula->data != IMPL)
         return false;
     if(newline->formula->data != NEG or newline->line2->formula->data != NEG)
         return false;
