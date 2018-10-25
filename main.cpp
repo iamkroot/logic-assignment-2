479a6ef4a9f0a1f7591dfe52afe9629e18e41066
Niral <f20170130@hyderabad.bits-pilani.ac.in>
2018-10-26 02:54:29 +0530

    Modify main.cpp for using particular tasks

--- main.cpp
+++ main.cpp
@@ -5,14 +5,28 @@
 using namespace std;
 
 int main(){
-    string infix;
-    cout<<"Enter the infix expression:\t";
-    getline(cin, infix);
-    string pf = infix_to_postfix(infix);
-    cout<<"Postfix: \t\t\t\t\t"<<pf<<endl;
-    BTree* root = postfix_to_parse_tree(&pf);
-    cout<<"Parse tree infix: \t\t\t"<<parse_tree_to_infix(root);
-    cout<<'\n';
-    validate_proof();
-    return 0;
+    int part;
+    cout<<"Logic Assignment 2"<<endl;
+    cout<<"Part 1: Infix->postfix->parse_tree->infix converter "<<endl;
+    cout<<"Part 2: A tool for verifying whether a certain proof of a given sequent is valid or not"<<endl;
+    cout<<"Enter Part number to run it:\t";
+    cin>>part;
+    if(part==1) {
+        string infix;
+        cout << "Enter the infix expression:\t";
+        getline(cin, infix);
+        string pf = infix_to_postfix(infix);
+        cout << "Postfix: \t\t\t\t\t" << pf << endl;
+        BTree *root = postfix_to_parse_tree(&pf);
+        cout << "Parse tree infix: \t\t\t" << parse_tree_to_infix(root);
+        cout << '\n';
+        return 0;
+    }
+    else if(part==2) {
+        validate_proof();
+        return 0;
+    }
+    else {
+        cout<<"Invalid Input"<<endl;
+    }
 }
